0e77d72f44e9ce43dcb4cfc220b98a87d6524f8c
GrzegorzDabrowski <70518773+GrzegorzDabrowski@users.noreply.github.com>
2021-01-12 15:18:54 +0100

    calculator added

--- js/app.js
+++ js/app.js
@@ -27,6 +27,8 @@ function Calculator(form, summary) {
       price: summary.querySelector(".total__price"),
     },
   };
+
+  this.addEvents();
 }
 
 Calculator.prototype.updateSummary = function (id, calc, total, callback) {
@@ -53,18 +55,107 @@ Calculator.prototype.inputEvent = function (e) {
   const singlePrice = this.prices[id];
   const totalPrice = value * singlePrice;
 
-  this.updateSummary(id, value + " * $" + singlePrice, totalPrice, function (
-    item,
-    calc,
-    total
-  ) {
-    if (value < 0) {
-      calc.innerHTML = null;
-      total.innerText = "Value should be greater than 0";
-    }
+  this.updateSummary(
+    id,
+    value + " * $" + singlePrice,
+    totalPrice,
+    function (item, calc, total) {
+      if (value < 0) {
+        calc.innerHTML = null;
+        total.innerText = "Value should be greater than 0";
+      }
 
-    if (value.length === 0) {
+      if (value.length === 0) {
+        item.classList.remove("open");
+      }
+    }
+  );
+
+  this.updateTotal();
+};
+
+Calculator.prototype.selectEvent = function (e) {
+  this.form.package.classList.toggle("open");
+
+  const value =
+    typeof e.target.dataset.value !== "undefined" ? e.target.dataset.value : "";
+  const text =
+    typeof e.target.dataset.value !== "undefined"
+      ? e.target.innerText
+      : "Choose package";
+
+  if (value.length > 0) {
+    this.form.package.dataset.value = value;
+    this.form.package.querySelector(".select__input").innerText = text;
+
+    this.updateSummary("package", text, this.prices.package[value]);
+  }
+
+  this.updateTotal();
+};
+
+Calculator.prototype.checkboxEvent = function (e) {
+  const checkbox = e.currentTarget;
+  const id = checkbox.id;
+  const checked = e.currentTarget.checked;
+
+  this.updateSummary(id, undefined, this.prices[id], function (item) {
+    if (!checked) {
       item.classList.remove("open");
     }
   });
+
+  this.updateTotal();
 };
+
+Calculator.prototype.addEvents = function () {
+  this.form.products.addEventListener("change", this.inputEvent.bind(this));
+  this.form.products.addEventListener("keyup", this.inputEvent.bind(this));
+  this.form.orders.addEventListener("change", this.inputEvent.bind(this));
+  this.form.orders.addEventListener("keyup", this.inputEvent.bind(this));
+
+  this.form.package.addEventListener("click", this.selectEvent.bind(this));
+
+  this.form.accounting.addEventListener(
+    "change",
+    this.checkboxEvent.bind(this)
+  );
+  this.form.terminal.addEventListener("change", this.checkboxEvent.bind(this));
+};
+
+Calculator.prototype.updateTotal = function () {
+  const show = this.summary.list.querySelectorAll(".open").length > 0;
+
+  if (show) {
+    const productSum =
+      this.form.products.value < 0
+        ? 0
+        : this.form.products.value * this.prices.products;
+    const ordersSum =
+      this.form.orders.value < 0
+        ? 0
+        : this.form.orders.value * this.prices.orders;
+    const packagePrice =
+      this.form.package.dataset.value.length === 0
+        ? 0
+        : this.prices.package[this.form.package.dataset.value];
+    const accounting = this.form.accounting.checked
+      ? this.prices.accounting
+      : 0;
+    const terminal = this.form.terminal.checked ? this.prices.terminal : 0;
+
+    this.summary.total.price.innerText =
+      "$" + (productSum + ordersSum + packagePrice + accounting + terminal);
+
+    this.summary.total.container.classList.add("open");
+  } else {
+    this.summary.total.container.classList.remove("open");
+  }
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector(".calc__form");
+  const summary = document.querySelector(".calc__summary");
+
+  new Calculator(form, summary);
+});
